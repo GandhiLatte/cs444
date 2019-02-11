698495b25dd319de6f7376aa74f8e644de34e02a
no <no>
2019-02-11 13:35:12 -0500

    Done-ish

--- exec.c
+++ exec.c
@@ -45,7 +45,7 @@ int handle_builtin(struct cmd *c)
 			{
 				chdir(c->exec.argv[1]); // this needs to be completed
 			}
-			return  1;
+			return 1;
 		}
 	default:
 		return 0;
@@ -91,20 +91,26 @@ int exec_cmd(struct cmd *c)
 	 * stays around afterward? (Hint: fork, then execvp, then wait; look
 	 * at main() for some inspiration.) */
 		child = fork1();
-		if(child == 0)
+		if (child == 0)
 		{
-			if(execvp(c->exec.argv[0], c->exec.argv) == -1){
-			} else
+			if (execvp(c->exec.argv[0], c->exec.argv) == -1)
+			{
+				exit(0);
+			}
+			else
 			{
 				exit(execvp(c->exec.argv[0], c->exec.argv));
 			}
-		} else {
+		}
+		else
+		{
 			int code;
 
-		/* Avoid spurious deaths--wait for specifically the child with the given PID
+			/* Avoid spurious deaths--wait for specifically the child with the given PID
 		 * to die. This *should* be our only child, but it doesn't hurt to be safe. */
-        	while(wait(&code) != child)
-            	;
+			while (wait(&code) != child)
+				;
+			return code;
 		}
 		break;
 
@@ -112,9 +118,39 @@ int exec_cmd(struct cmd *c)
 		/* TODO: Run *two* commands, but with the stdout chained to the stdin
 	 * of the next command. How? Use the syscall pipe: */
 		{
+			int child2;
 			int pipefds[2];
 			pipe(pipefds);
+			child = fork1();
 
+			if (child == 0)
+			{
+				dup2(pipefds[1], 1);
+				close(pipefds[0]);
+				close(pipefds[1]);
+				exec_cmd(c->pipe.left);
+			}
+			else
+			{
+				child2 = fork1();
+				if (child2 == 0)
+				{
+					dup2(pipefds[0], 0);
+					close(pipefds[0]);
+					close(pipefds[1]);
+					exec_cmd(c->pipe.right);
+				}
+				else
+				{
+					int code;
+					int code2;
+					close(pipefds[0]);
+					close(pipefds[1]);
+					waitpid(child, &code, 0);
+					waitpid(child2, &code2, 0);
+					return code2;
+				}
+			}
 
 
 			/* At this point, pipefds[0] is a read-only FD, and pipefds[1] is a
@@ -131,16 +167,44 @@ int exec_cmd(struct cmd *c)
 		break;
 
 	case REDIR:
-	 	// c->redir.______  whatever
+		// c->redir.______  whatever
 		/* Redirect a file descriptor, then use it in the command. Note that you
 	 * should avoid changing the *shell's* file descriptors, so you probably
 	 * want to do this in a process. As a hint, look back at EXEC when you
 	 * have that done. */
+		child = fork1();
+		if (child == 0)
+		{
+			int fd = open(c->redir.path, c->redir.mode);
+			if (fd < 0)
+			{
+				exit(0);
+			}
+			else
+			{
+				dup2(fd, c->redir.fd);
+				close(fd);
+				exit(exec_cmd(c->redir.cmd));
+			}
+		}
+		else
+		{
+			int code;
+			while (wait(&code) != child)
+				;
+		}
+
 		break;
 
 	case BACK:
 		/* This should be easy--what's something you can remove from EXEC to have
 	 * this function return before the child exits? */
+
+		child = fork1();
+		if (child == 0)
+		{
+			exit(exec_cmd(c->back.cmd));
+		}
 		break;
 
 	default:
